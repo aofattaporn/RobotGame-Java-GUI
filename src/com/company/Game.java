6ec62e3388bf3619d809c8efaa2e99a3f2a4d257
Attaporn Peungsook <aofattapon321@gmail.com>
2022-02-27 00:48:32 +0700

    add ID

--- src/com/company/Game.java
+++ src/com/company/Game.java
@@ -23,8 +23,8 @@ public class Game extends Canvas implements Runnable {
 
         handler = new Handler();
 
-        handler.addObject(new Box(100, 100));
-        handler.addObject(new Box(2g00, 100));
+        handler.addObject(new Box(100, 100, ID.Block));
+//        handler.addObject(new Box(200, 100, ID.player));
 
     }
 
